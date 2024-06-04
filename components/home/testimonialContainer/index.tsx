ff6d06ce6964254d6ebdee6c7c83da8cc1157da8
kkm980 <kantkrishna751@gmail.com>
2024-06-04 19:54:06 +0530

    updated changes

--- components/home/testimonialContainer/index.tsx
+++ components/home/testimonialContainer/index.tsx
@@ -5,12 +5,12 @@ import {RenderTranslatedText} from "@/components/common/translateText";
 const TestimonialContainer = ({index}:{index:number}): JSX.Element => {
 
     return (
-        <div className="relative mt-[80px] w-[475px] text-center text-bold text-[24px] text-white leading-[140%]">
+        <div className="relative mt-[80px] w-[475px] text-center text-bold text-[24px] text-[#AAD9D1] leading-[140%]">
             <div>
                 <RenderTranslatedText componentArray={["home","testimonials",`${index}`, "text"]}/>
             </div>
             <Image src="/home/testimonial/quote.svg" width={38} height={25} alt="quote" className="absolute -left-6 -top-4" />
-            <div className="mt-4 flex justify-center items-center font-regular text-[16px]">
+            <div className="mt-4 flex justify-center items-center font-regular text-[16px] text-titleDesc">
                 <div className="mr-[8px]">
                     <RenderTranslatedText componentArray={["home","testimonials",`${index}`, "author"]}/>
                 </div>
