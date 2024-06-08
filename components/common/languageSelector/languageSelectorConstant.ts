b60b2f8467bfa321017a57cedc98c05f2b92d55e
kkm980 <kantkrishna751@gmail.com>
2024-06-08 11:34:40 +0530

    fixed lint errors

--- components/common/languageSelector/languageSelectorConstant.ts
+++ components/common/languageSelector/languageSelectorConstant.ts
@@ -15,45 +15,46 @@ export const locales: Locales = {
     title: "Arabic",
     flagIcon: "/images/common/flags/ar.svg",
   },
-  bg: {
-    title: "Bulgarian",
-    flagIcon: "/images/common/flags/bg.svg",
+  
+  bl: {
+    title: "Bengali",
+    flagIcon: "/images/common/flags/in.svg",
   },
   bj: {
     title: "Bhojpuri",
     flagIcon: "/images/common/flags/in.svg",
   },
-  bl: {
-    title: "Bengali",
-    flagIcon: "/images/common/flags/in.svg",
-  },
-  zh_cn: {
-    title: "Chinese (simplified)",
-    flagIcon: "/images/common/flags/cn.svg",
+  bg: {
+    title: "Bulgarian",
+    flagIcon: "/images/common/flags/bg.svg",
   },
   zh_tw: {
     title: "Chinese (traditional)",
+    flagIcon: "/images/common/flags/cn.svg",
+  },
+  zh_cn: {
+    title: "Chinese (simplified)",
     flagIcon: "/images/common/flags/cn.svg",
   },
   cz: {
     title: "Czech",
     flagIcon: "/images/common/flags/cz.svg",
   },
-  de: {
-    title: "German",
-    flagIcon: "/images/common/flags/de.svg",
+  nl: {
+    title: "Dutch",
+    flagIcon: "/images/common/flags/nl.svg",
   },
   en: {
     title: "English",
     flagIcon: "/images/common/flags/en.svg",
   },
-  es: {
-    title: "Spanish",
-    flagIcon: "/images/common/flags/es.svg",
-  },
   fr: {
     title: "French",
     flagIcon: "/images/common/flags/fr.svg",
+  },
+  de: {
+    title: "German",
+    flagIcon: "/images/common/flags/de.svg",
   },
   gj: {
     title: "Gujarati",
@@ -87,10 +88,6 @@ export const locales: Locales = {
     title: "Korean",
     flagIcon: "/images/common/flags/kr.svg",
   },
-  mr: {
-    title: "Marathi",
-    flagIcon: "/images/common/flags/in.svg",
-  },
   mt: {
     title: "Maithili",
     flagIcon: "/images/common/flags/in.svg",
@@ -99,29 +96,29 @@ export const locales: Locales = {
     title: "Malayalam",
     flagIcon: "/images/common/flags/in.svg",
   },
-  nl: {
-    title: "Dutch",
-    flagIcon: "/images/common/flags/nl.svg",
+  mr: {
+    title: "Marathi",
+    flagIcon: "/images/common/flags/in.svg",
   },
   od: {
     title: "Odiya",
     flagIcon: "/images/common/flags/in.svg",
   },
-  pj: {
-    title: "Punjabi",
+  ps: {
+    title: "Pashto",
     flagIcon: "/images/common/flags/in.svg",
   },
   pl: {
     title: "Polish",
     flagIcon: "/images/common/flags/pl.svg",
   },
-  ps: {
-    title: "Pashto",
-    flagIcon: "/images/common/flags/in.svg",
-  },
   pt: {
     title: "Portuguese",
     flagIcon: "/images/common/flags/pt.svg",
+  },
+  pj: {
+    title: "Punjabi",
+    flagIcon: "/images/common/flags/in.svg",
   },
   rj: {
     title: "Rajasthani",
@@ -135,33 +132,37 @@ export const locales: Locales = {
     title: "Russian",
     flagIcon: "/images/common/flags/ru.svg",
   },
+  sn: {
+    title: "Sanskrit",
+    flagIcon: "/images/common/flags/in.svg",
+  },
   sk: {
     title: "Slovak",
     flagIcon: "/images/common/flags/sk.svg",
   },
-  sn: {
-    title: "Sanskrit",
-    flagIcon: "/images/common/flags/in.svg",
+  es: {
+    title: "Spanish",
+    flagIcon: "/images/common/flags/es.svg",
   },
   sw: {
     title: "Swedish",
     flagIcon: "/images/common/flags/sw.svg",
   },
-  th: {
-    title: "Thai",
-    flagIcon: "/images/common/flags/th.svg",
-  },
   tl: {
     title: "Tamil",
     flagIcon: "/images/common/flags/in.svg",
   },
-  tr: {
-    title: "Turkish",
-    flagIcon: "/images/common/flags/tr.svg",
-  },
   tu: {
     title: "Telagu",
     flagIcon: "/images/common/flags/in.svg",
+  },
+  th: {
+    title: "Thai",
+    flagIcon: "/images/common/flags/th.svg",
+  },
+  tr: {
+    title: "Turkish",
+    flagIcon: "/images/common/flags/tr.svg",
   },
   ud: {
     title: "Urdu",
